82c78cfca532d666f658138a5897d6559c992b8a
roxanne-tampus <51393035+roxanne-tampus@users.noreply.github.com>
2022-09-22 10:25:19 +0800

    added model

---
+++ models/subscription.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Subscription struct {
+	Id              int64     `json:"id"`
+	Name            string    `json:"name"`
+	CreatedDate     time.Time `json:"created_date"`
+	Email_Templates int64     `json:"email_template"`
+	Capture_Pages   int64     `json:"email_pages"`
+}
